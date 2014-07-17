47e74bb0b2afc0aca9dbd3cd6232a2faa0f2d82a
Anthonyas007 <Anthony.Schweigard@GMail.com>
2014-07-16 21:21:27 -0500

    test commit

---
+++ hackathon-test-core/src/main/java/hello/HelloController.java
@@ -0,0 +1,21 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index()
+    {
+        return "Greetings from Spring Boot!";
+    }
+    
+    @RequestMapping(value = "/helloJSON")
+    public String getJSON()
+    {
+		return "{\"text\" : \"Hello, World!\"}";
+    }
+
+}
